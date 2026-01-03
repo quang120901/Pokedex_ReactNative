aa7d348c8d0e13537d8f94e3bd6660dd885f3284
Nguyen Nhat Quang <nguyennhatquang963@gmail.com>
2026-01-03 23:58:10 +0700

    test api

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+  useEffect(() => {
+
+  }, [])
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20"
+      );
+      const data = await response.json();
+    } catch (e) {
+      console.log(e)
+    }
+  }
   return (
     <View
       style={{
