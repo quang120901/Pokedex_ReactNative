a619255ae7f5e670f8dba477c5864a73056e7a3b
Nguyen Nhat Quang <nguyennhatquang963@gmail.com>
2026-01-05 15:49:05 +0700

    adding image

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 
 
 interface Pokemon {
   name: string;
-  url: string;
+  image: string;
 }
 
 export default function Index() {
@@ -18,11 +18,22 @@ export default function Index() {
   async function fetchPokemons() {
     try {
       const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon/?limit=20"
+        "https://pokeapi.co/api/v2/pokemon/?limit=10"
       );
       const data = await response.json();
 
-      setPokemons(data.results);
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default,
+          };
+        })
+      );
+
+      setPokemons(detailedPokemons);
     } catch (e) {
       console.log(e)
     }
@@ -33,6 +44,8 @@ export default function Index() {
       {pokemons.map((pokemon) => (
         <View key={pokemon.name}>
           <Text>{pokemon.name}</Text>
+          <Image source={{ uri: pokemon.image }}
+            style={{ width: 100, height: 100 }} />
         </View>
       ))}
     </ScrollView>
